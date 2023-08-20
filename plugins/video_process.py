292fb26708e51d6bef5101bb3886dbddaf6bcb64
crazebotz <120011436+crazebotz@users.noreply.github.com>
2023-08-20 08:22:59 +0530

    Update video_process.py

--- plugins/video_process.py
+++ plugins/video_process.py
@@ -1,26 +1,25 @@
-import random
 from moviepy.video.io.VideoFileClip import VideoFileClip
+import random
 from PIL import Image
 
 
 def video_data(video_path, output_path):
-    video_clip = VideoFileClip(video_path)
-    video_duration = video_clip.duration
-    random_time = random.uniform(1, video_duration)
 
-    screenshot = video_clip.get_frame(random_time)
-    pil_image = Image.fromarray(screenshot)
-    pil_image.save(output_path)
-    # screenshot.save_frame(output_path)
-    width = video_clip.size[0]  # Width
-    height = video_clip.size[1]  # Height
-    return output_path, width, height, video_duration
-
-
-# async def get_vid_info(image_path):
-#     try:
-#         with Image.open(image_path) as img:
-#             width, height = img.size
-#             return width, height
-#     except Exception as e:
-#         return 320, 320
+    try:
+        video_clip = VideoFileClip(video_path)
+        print(10)
+        video_duration = video_clip.duration
+        random_time = random.uniform(1, (round(video_duration)-1))
+        print(13)
+        screenshot = video_clip.get_frame(random_time)
+        print(15)
+        pil_image = Image.fromarray(screenshot)
+        print(17)
+        pil_image.save(output_path)
+        width = video_clip.size[0]  # Width
+        height = video_clip.size[1]  # Height
+        print(21)
+        return output_path, width, height, video_duration
+    except Exception as e:
+        print("An error occurred:", str(e))
+        return None, None, None, None
